f6015b95efda096c5325a08c0479e5ae8d2a1f57
DharaRioja <cuatita_16@hotmail.com>
2017-04-26 20:45:05 -0400

    first commit

---
+++ SimpleScheduling/src/com/scheduling/simple/model/Class.java
@@ -0,0 +1,84 @@
+/*
+ * Class.java
+ */
+package com.scheduling.simple.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Class")
+public class Class {
+    
+    @XmlElement(name = "code")
+    private String code;
+    @XmlElement(name = "title")
+    private String title;
+    @XmlElement(name = "description")
+    private String description;
+    @XmlElement(name = "ids")
+    private List<Integer> ids;
+
+    public Class(String code, String title, String description) {
+       this.code = code;
+       this.title = title;
+       this.description = description;
+       ids = new ArrayList<Integer>();
+    }
+
+    /**
+     * @return the code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public void addId(int id)
+    {
+        ids.add(id);
+    }
+    
+    public List<Integer> getIds()
+    {
+        return ids;
+    }
+}
---
+++ SimpleScheduling/src/com/scheduling/simple/ws/SimpleSchedulingResource.java
@@ -0,0 +1,95 @@
+/*
+ * SimpleSchedulingResource.java
+ */
+package com.scheduling.simple.ws;
+
+import com.scheduling.simple.model.Student;
+import com.scheduling.simple.model.Class;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SimpleSchedulingResource {
+    
+    private Map<Integer, Student> studentsMap;
+    private Map<String, Class> classesMap;
+
+    public SimpleSchedulingResource() {
+        studentsMap = new HashMap<Integer, Student>();
+        classesMap = new HashMap<String, Class>();
+    }
+    
+    public Student createStudent(int studentId, String lastName, String firstName) {
+        Student student = new Student(studentId, lastName, firstName);
+        studentsMap.put(studentId, student);
+        
+        return student;
+    }
+    
+    public Class createClass(String code, String title, String description) {
+        
+        Class clas = new Class(code, title, description);
+        classesMap.put(code, clas);
+        
+        return clas;
+        
+    }
+
+    public Student retrieveStudent(int studentId) {
+        return studentsMap.get(studentId);
+    }
+    
+    public Class retrieveClass(String code){
+        return classesMap.get(code);
+    }
+    
+    public void editStudent(int studentId, Student student){
+        studentsMap.replace(studentId, student);
+    }
+    
+    public void editClassTitle(String code, String title){
+        classesMap.get(code).setTitle(title);
+    }
+    
+    public void editClassDescription(String code, String description){
+        classesMap.get(code).setDescription(description);
+    }
+    
+    public void editStundentFirstName(int studentId, String firstName){
+        studentsMap.get(studentId).setFirstName(firstName);
+    }
+    
+    public void editStudentLastName(int studentId, String lastName){
+        studentsMap.get(studentId).setLastName(lastName);
+    }
+    
+    public void deleteStudent(int studentId)
+    {
+        studentsMap.remove(studentId);
+    }
+    
+    public void deleteClass(String code)
+    {
+        classesMap.remove(code);
+    }
+    
+    public void addStudentToClass(int studentId, String code)
+    {
+        classesMap.get(code).addId(studentId);
+        studentsMap.get(studentId).addCode(code);
+    }
+    
+    public List<Student> getStudents()
+    {
+        List<Student> students = new ArrayList<>(studentsMap.values());
+        return students;
+    }
+    
+    public List<Class> getClasses()
+    {
+        List<Class> classes = new ArrayList<>(classesMap.values());
+        return classes;
+    }
+    
+}
